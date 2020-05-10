d56d4c04f50d9d1406e96a7a5b95a8f8fe8621e0
YannisFalco <yannis.falco@etu.unice.fr>
2020-05-10 11:14:27 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+## Building client
+cd front
+mvn clean install
+cd ..
+
+## Building the j2e system
+cd j2e-entities
+mvn clean install
+cd ..
+
+cd j2e-otherComponents
+mvn clean install
+cd ..
+
+cd j2e
+mvn clean install
+cd ..
+
+## Building the .Net system
+cd dotNet
+./compile.sh
+cd ..
