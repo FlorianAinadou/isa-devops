c5a8945693231ddb75f0e0fa26a086b4604828e8
remyLarroye <remy.larroye@stu.unice.fr>
2020-05-10 15:46:40 +0200

    Ajout du docker-compose.yml

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 ## Building the j2e system
 cd j2e-entities
 mvn clean install
+docker build -t dd-j2e .
 cd ..
+
 
 ## Building client
 cd front
@@ -16,9 +18,13 @@ cd ..
 
 cd j2e
 mvn clean install
+docker build -t dd-client .
 cd ..
+
 
 ## Building the .Net system
 cd dotNet
 ./compile.sh
+docker build -t dd-dotnet .
 cd ..
+
